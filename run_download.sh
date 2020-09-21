7daaa4078e1dc6bc5efb6d7279837a84f41620c8
rai-xavier <xgutierrez@reality.ai>
2020-09-21 09:26:13 -0400

    example calls

---
+++ run_download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# cd venv2/Scripts/
+# activate
+# cd ../../
+# python2 automate_download_freesound.py "baby crying" --download-dir C:\wrk\bose-babycry\test --license "Attribution"
+python2 automate_download_freesound.py "baby crying" --download-dir C:\wrk\bose-babycry\test --license "Creative Commons 0"
+python2 automate_download_freesound.py "baby crying" --download-dir C:\wrk\bose-babycry\test --license "Sampling+"
